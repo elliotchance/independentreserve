89fd322dd31ca0ff20ea4cc0b679f7df01318700
Elliot Chance <elliotchance@gmail.com>
2014-12-23 22:07:27 +1100

    testGetValidSecondaryCurrencyCodes

--- src/IndependentReserve/IndependentReserveClient.php
+++ src/IndependentReserve/IndependentReserveClient.php
@@ -2,12 +2,50 @@
 
 namespace IndependentReserve;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Response;
+
 class IndependentReserveClient
 {
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    /**
+     * @param string $url
+     * @return mixed
+     */
+    protected function get($url)
+    {
+        /** @noinspection PhpVoidFunctionResultUsedInspection */
+        /** @var Response $response */
+        $response = $this->client->get($url);
+        return json_decode($response->getBody());
+    }
+
+    /**
+     * Returns a list of valid primary currency codes. These are the digital currencies which can be
+     * traded on Independent Reserve.
+     * @return array
+     */
     public function getValidPrimaryCurrencyCodes()
     {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->get('https://api.independentreserve.com/Public/GetValidPrimaryCurrencyCodes');
-        return json_decode($response->getBody());
+        return $this->get('https://api.independentreserve.com/Public/GetValidPrimaryCurrencyCodes');
+    }
+
+    /**
+     * Returns a list of valid secondary currency codes. These are the fiat currencies which are
+     * supported by Independent Reserve for trading purposes.
+     * @return array
+     */
+    public function getValidSecondaryCurrencyCodes()
+    {
+        return $this->get('https://api.independentreserve.com/Public/GetValidSecondaryCurrencyCodes');
     }
 }
--- tests/integration/IndependentReserve/IndependentReserveClientIntegrationTest.php
+++ tests/integration/IndependentReserve/IndependentReserveClientIntegrationTest.php
@@ -6,10 +6,26 @@ use Concise\TestCase;
 
 class IndependentReserveClientIntegrationTest extends TestCase
 {
+    /**
+     * @var IndependentReserveClient
+     */
+    protected $client;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->client = new IndependentReserveClient();
+    }
+
     public function testGetValidPrimaryCurrencyCodes()
     {
-        $client = new IndependentReserveClient();
-        $currencyCodes = $client->getValidPrimaryCurrencyCodes();
+        $currencyCodes = $this->client->getValidPrimaryCurrencyCodes();
         $this->assert($currencyCodes, equals, ["Xbt"]);
+    }
+
+    public function testGetValidSecondaryCurrencyCodes()
+    {
+        $currencyCodes = $this->client->getValidSecondaryCurrencyCodes();
+        $this->assert($currencyCodes, has_value, "Usd");
     }
 }
